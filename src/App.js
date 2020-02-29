4afc41a3598c8d0ff13a9d2e0c55aeade116a35f
Ira <ira.d.sanchez@gmail.com>
2020-02-29 01:09:28 -0700

    Add Home Page and Routes For Home, Login, and Reg

--- src/App.js
+++ src/App.js
@@ -1,17 +1,26 @@
+//dependencies
 import React from "react";
+import { Route } from "react-router-dom";
+
+//components
+import Home from "./Home/Home";
 import RegisterForm from "./Auth/RegisterForm";
 import LoginForm from "./Auth/LoginForm";
-import NewGameButton from "./GamePlay/NewGameButton";
-import MoveButtons from "./GamePlay/Movement/MoveButtons";
+
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <RegisterForm />
-      <LoginForm />
-      <NewGameButton />
-      <MoveButtons />
+      <Route exact path="/">
+        <Home />
+      </Route>
+      <Route path="/login">
+        <LoginForm />
+      </Route>
+      <Route path="/register">
+        <RegisterForm />
+      </Route>
     </div>
   );
 }
