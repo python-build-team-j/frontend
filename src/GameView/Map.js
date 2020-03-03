e2bc46f0872f80654f8cf8b41ef1dfbb313dcfc9
Ira <ira.d.sanchez@gmail.com>
2020-03-02 19:55:23 -0700

    Map over room data in Map component
    - uses dummy data

--- src/GameView/Map.js
+++ src/GameView/Map.js
@@ -4,7 +4,59 @@ import axios from "axios";
 
 const Map = props => {
   const [rooms, setRooms] = useState([]);
-
+  const dummyData = [
+    {
+      model: "adventure.room",
+      pk: 1,
+      fields: {
+        title: "Outside Cave Entrance",
+        description: "North of you, the cave mount beckons",
+        n_to: 2,
+        s_to: 0,
+        e_to: 0,
+        w_to: 0
+      }
+    },
+    {
+      model: "adventure.room",
+      pk: 3,
+      fields: {
+        title: "Grand Overlook",
+        description:
+          "A steep cliff appears before you, falling\ninto the darkness. Ahead to the north, a light flickers in\nthe distance, but there is no way across the chasm.",
+        n_to: 0,
+        s_to: 2,
+        e_to: 0,
+        w_to: 0
+      }
+    },
+    {
+      model: "adventure.room",
+      pk: 2,
+      fields: {
+        title: "Foyer",
+        description:
+          "Dim light filters in from the south. Dusty\npassages run north and east.",
+        n_to: 3,
+        s_to: 1,
+        e_to: 4,
+        w_to: 0
+      }
+    },
+    {
+      model: "adventure.room",
+      pk: 4,
+      fields: {
+        title: "Narrow Passage",
+        description:
+          "The narrow passage bends here from west\nto north. The smell of gold permeates the air.",
+        n_to: 5,
+        s_to: 0,
+        e_to: 0,
+        w_to: 2
+      }
+    }
+  ];
   useEffect(() => {
     axios
       .get("https://lambda-mud-test.herokuapp.com/api/adv/rooms/")
@@ -17,8 +69,10 @@ const Map = props => {
 
   return (
     <div>
-      <button onClick={props.handleModal}>X</button>Map of the world here
-      {/* Map over room data here */}
+      <button onClick={props.handleModal}>X</button>
+      {dummyData.map(room => {
+        return <div>{room.fields.title}</div>;
+      })}
     </div>
   );
 };
